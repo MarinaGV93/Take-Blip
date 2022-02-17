b73fa320b94fa6f6e4ee9ab8bf926d6787715e32
Marina Golao Vale <marinasgv@hotmail.com>
2022-02-17 14:41:26 -0300

    Atualizaçao

--- Contatdor/assets/js/scripts.js
+++ Contatdor/assets/js/scripts.js
@@ -1,18 +1,36 @@
-// Armazenando um valor q esta no Span
-var currentNumberWrapper = document.getElementById('currentNumber');
+// // Armazenando um valor q esta no Span
+// var currentNumberWrapper = document.getElementById('currentNumber');
 
-var currentNumber = 0;
+// var currentNumber = 0;
 
-function increment(){
-    // Incrmentar a var
-    currentNumber = currentNumber + 1;
-    // innerHTML = o HTML que esta dentro da var
-    currentNumberWrapper.innerHTML = currentNumber;
+// function increment(){
+//     // Incrmentar a var
+//     currentNumber = currentNumber + 1;
+//     // innerHTML = o HTML que esta dentro da var
+//     currentNumberWrapper.innerHTML = currentNumber;
+// }
+
+// function decrement(){
+//     // Incrmentar a var
+//     currentNumber = currentNumber - 1;
+//     // innerHTML = o HTML que esta dentro da var
+//     currentNumberWrapper.innerHTML = currentNumber;
+// }
+
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
 }
 
-function decrement(){
-    // Incrmentar a var
-    currentNumber = currentNumber - 1;
-    // innerHTML = o HTML que esta dentro da var
-    currentNumberWrapper.innerHTML = currentNumber;
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
 }
